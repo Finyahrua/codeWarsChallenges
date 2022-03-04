0ed4ea8fc3614d6c4b0a638c53dd2260faf85b1f
Finyahrua <finaddy@gmail.com>
2022-03-04 19:39:44 +0300

    next challenge complete, see the readme.txt for the instructions of the problem,leave reviews as you see fit.Thanks

---
+++ Multiples_of_three_or_five/Multiples_of_three_or_five.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(solution(10));
+console.log(solution(20));
+console.log(solution(30));
